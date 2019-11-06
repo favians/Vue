f1b3a299b625f1e0ffad28e7f0c57c00cae95bfd
faviansyah <faviansyah@aterra.id>
2019-11-06 22:26:34 +0700

    fixing all development

--- deploy/deploy-devel.sh
+++ deploy/deploy-devel.sh
@@ -1,3 +1,13 @@
 cd $DEVELOPMENT_APP_PATH
-pwd
-touch a.txt
+
+git checkout development
+git pull origin development
+git branch -D $DEVELOPMENT_BRANCH
+git fetch origin
+git checkout $DEVELOPMENT_BRANCH
+git pull origin $DEVELOPMENT_BRANCH
+
+cp /deploy/docker-compose-dev.yaml ./docker-compose.yaml
+
+sudo docker-compose down
+sudo docker-compose up --build
