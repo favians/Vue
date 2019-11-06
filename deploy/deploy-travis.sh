38d8d0ae00e8c0cb24c99ce600076aea2fde4eb8
faviansyah <faviansyah@aterra.id>
2019-11-06 18:35:12 +0700

    test doang

--- deploy/deploy-travis.sh
+++ deploy/deploy-travis.sh
@@ -1,8 +1,8 @@
 if [ ${TRAVIS_PULL_REQUEST} = "false" -a ${TRAVIS_BRANCH} = $DEVELOPMENT_BRANCH ]; then
-	openssl aes-256-cbc -k "${SSH_ENCRYPT_PASSWORD}" -in lucent.pem.enc -out lucent.pem -d
-	chmod 600 lucent.pem
-	ssh-add lucent.pem
+	openssl aes-256-cbc -k "${SSH_ENCRYPT_PASSWORD}" -in /home/travis/build/favians/Vue/deploy/lucent.pem.enc -out /home/travis/build/favians/Vue/deploy/lucent.pem -d
+	chmod 600 /home/travis/build/favians/Vue/deploy/lucent.pem
+	ssh-add /home/travis/build/favians/Vue/deploy/lucent.pem
 	ssh-keygen -R $DEVELOPMENT_HOST
 	ssh-keyscan -H $DEVELOPMENT_HOST >> ~/.ssh/known_hosts
-	ssh -v -i lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < deploy-devel.sh
+	ssh -v -i lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < /home/travis/build/favians/Vue/deploy/deploy-devel.sh
 fi
