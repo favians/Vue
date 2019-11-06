bc45954106dd34ef523e4ad4bfecc1f7fa9abccb
faviansyah <faviansyah@aterra.id>
2019-11-06 17:32:35 +0700

    add deploy state

--- deploy/deploy-travis.sh
+++ deploy/deploy-travis.sh
@@ -1,8 +1,8 @@
 if [ ${TRAVIS_PULL_REQUEST} = "false" -a ${TRAVIS_BRANCH} = $DEVELOPMENT_BRANCH ]; then
-	openssl aes-256-cbc -k "${SSH_ENCRYPT_PASSWORD}" -in ./lucent.pem.enc -out ./lucent.pem -d
-	chmod 600 ./lucent.pem
-	ssh-add ./lucent.pem
+	openssl aes-256-cbc -k "${SSH_ENCRYPT_PASSWORD}" -in lucent.pem.enc -out lucent.pem -d
+	chmod 600 lucent.pem
+	ssh-add lucent.pem
 	ssh-keygen -R $DEVELOPMENT_HOST
 	ssh-keyscan -H $DEVELOPMENT_HOST >> ~/.ssh/known_hosts
-	ssh -v -i lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < ./deploy-devel.sh
+	ssh -v -i lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < deploy-devel.sh
 fi
