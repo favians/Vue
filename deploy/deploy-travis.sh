5f6de815c17353b95eccf0961fa0fc886705a3f7
faviansyah <faviansyah@aterra.id>
2019-11-06 19:34:20 +0700

    change permission and some path

--- deploy/deploy-travis.sh
+++ deploy/deploy-travis.sh
@@ -4,5 +4,5 @@ if [ ${TRAVIS_PULL_REQUEST} = "false" -a ${TRAVIS_BRANCH} = $DEVELOPMENT_BRANCH 
 	ssh-add /home/travis/build/favians/Vue/deploy/lucent.pem
 	ssh-keygen -R $DEVELOPMENT_HOST
 	ssh-keyscan -H $DEVELOPMENT_HOST >> ~/.ssh/known_hosts
-	ssh -v -i lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < /home/travis/build/favians/Vue/deploy/deploy-devel.sh
+	ssh -v -i /home/travis/build/favians/Vue/deploy/lucent.pem $DEVELOPMENT_SSH_USER@$DEVELOPMENT_HOST DEVELOPMENT_APP_PATH=$DEVELOPMENT_APP_PATH DEVELOPMENT_BRANCH=$DEVELOPMENT_BRANCH 'bash -s' < /home/travis/build/favians/Vue/deploy/deploy-devel.sh
 fi
